7675f80209c400d9a6ec8470aadf064ea56c0c16
Aha <aha23@qq.com>
2022-07-02 11:45:18 +0800

    first-commit

---
+++ src/main/java/com/aha/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.aha.git;
+
+/**
+ * @author Aha
+ * @create 2022-07-02  11:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("I am aha!");
+    }
+}
