339ac69132123c152517b665f32e2f762aaaa7cc
Aha <aha23@qq.com>
2022-07-02 11:45:56 +0800

    second-commit

--- src/main/java/com/aha/git/GitTest.java
+++ src/main/java/com/aha/git/GitTest.java
@@ -7,5 +7,7 @@ package com.aha.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("I am aha!");
+        System.out.println("I am lxj!");
+
     }
 }
